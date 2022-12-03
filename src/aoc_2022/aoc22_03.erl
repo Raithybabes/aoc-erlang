25b50454eeae8ede15c2b60a9fa483de4150f6e8
Raith Munro <raith@ineffable.co.uk>
2022-12-03 09:50:52 +0000

    Extract intersect funs to rwm module

--- src/aoc_2022/aoc22_03.erl
+++ src/aoc_2022/aoc22_03.erl
@@ -2,10 +2,6 @@
 
 -export([answer/0]).
 
-intersect(ListA, ListB) -> lists:uniq([A || A <- ListA, B <- ListB, A =:= B]).
-
-intersect([List1 | T]) -> lists:foldl(fun (L, A) -> intersect(L, A) end, List1, T).
-
 group(L) -> group(L, []).
 group([], Groups) -> Groups;
 group([G1, G2, G3 | T], Groups) -> group(T, [[G1, G2, G3] | Groups]).
@@ -19,6 +15,6 @@ sum_priorities(L) -> lists:sum([priority(A) || A <- L]).
 answer() ->
     Dat = aoc22:data("03", fun process_dat/1),
     Compartments = [lists:split(floor(length(Rucksack) / 2), Rucksack) || Rucksack <- Dat],
-    RucksackIntersects = [intersect(List1, List2) || {List1, List2} <- Compartments],
-    GroupIntersects = [intersect(Group) || Group <- group(Dat)],
+    RucksackIntersects = [rwm:intersect(List1, List2) || {List1, List2} <- Compartments],
+    GroupIntersects = [rwm:intersect(Group) || Group <- group(Dat)],
     {sum_priorities(RucksackIntersects), sum_priorities(GroupIntersects)}.
--- src/rwm_lib/rwm.erl
+++ src/rwm_lib/rwm.erl
@@ -2,7 +2,7 @@
 
 -export([iif/1, iif/3]).
 -export([string_to_atoms/1, strings_to_atoms/1]).
--export([sort_lists_by_length/1, first/1, first/2, last/1, last/2]).
+-export([sort_lists_by_length/1, first/1, first/2, last/1, last/2, intersect/1, intersect/2]).
 -export([frequencies/1]).
 -export([pipe/2]).
 -export([pipeline/2]).
@@ -49,3 +49,7 @@ last(List) ->
 last(N, L) ->
     {_, Keep} = lists:split(length(L) - N, L),
     Keep.
+
+intersect(ListA, ListB) -> lists:uniq([A || A <- ListA, B <- ListB, A =:= B]).
+
+intersect([List1 | T]) -> lists:foldl(fun (L, A) -> intersect(L, A) end, List1, T).
