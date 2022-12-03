f280dda2e79abac754017dac33bd1a4163107596
Raith Munro <raith.munro@vonage.com>
2022-12-03 00:05:54 +0000

    AOC21 11 solved

---
+++ src/aoc_2021/aoc21_11.erl
@@ -0,0 +1,49 @@
+-module(aoc21_11).
+
+-export([answer/0]).
+
+-define(else, true).
+
+octopus_step({X, Y, _V_STALE, Grid, Ext}) ->
+    V = grid:get(X, Y, Grid),
+    if
+        V < 9 ->
+            {grid:set(X, Y, V + 1, Grid), Ext};
+        V =:= 9 ->
+            NewGrid1 = grid:set(X, Y, x, Grid),
+            Neighbours = grid:adjacent_and_diagonal_neighbours(X, Y, Grid),
+            {NewGrid2, _} = lists:foldl(
+                fun({NX, NY, _}, {NGrid, NExt}) -> octopus_step({NX, NY, null, NGrid, NExt}) end,
+                {NewGrid1, Ext},
+                Neighbours
+            ),
+            {NewGrid2, Ext};
+        true ->
+            {Grid, Ext}
+    end.
+
+octopus_flash({X, Y, V, Grid, {Steps, Flashes}}) ->
+    if
+        V =:= x -> {grid:set(X, Y, 0, Grid), {Steps, Flashes + 1}};
+        ?else -> {Grid, {Steps, Flashes}}
+    end.
+
+process_flashes({Grid, {Steps, Flashes}}) ->
+    grid:process_ext(fun octopus_flash/1, Grid, {Steps + 1, Flashes}).
+
+finish_when_all_flash({{W, H, _G} = Grid, {Steps, Flashes}}) ->
+    IsFinished = rwm:iif((Flashes =:= (W * H)) or (Steps =:= 1000)),
+    {Grid, {Steps, 0}, IsFinished}.
+
+process_dat(Line) -> [list_to_integer(X) || X <- dat:match_global(Line, "\\d")].
+
+answer() ->
+    Dat = aoc21:data("11", fun process_dat/1),
+    Grid = grid:from_list2d(Dat),
+    {_, {_Steps, FlashesAfter100Steps}} = grid:process_ext_times(
+        fun octopus_step/1, fun process_flashes/1, Grid, {0, 0}, 100
+    ),
+    {_, {StepsToAllFlash, _Flashes}} = grid:process_ext_until(
+        fun octopus_step/1, fun process_flashes/1, fun finish_when_all_flash/1, Grid, {0, 0}
+    ),
+    {FlashesAfter100Steps, StepsToAllFlash}.
