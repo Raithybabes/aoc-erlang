369825a8abb1cb408e2c5fdf245ec52d4ca7b813
Raith Munro <raith@ineffable.co.uk>
2022-12-06 08:57:11 +0000

    AOC22 05 fully parse the input data

--- src/aoc_2022/aoc22_05.erl
+++ src/aoc_2022/aoc22_05.erl
@@ -2,31 +2,44 @@
 
 -export([answer/0]).
 
-process_dat(Line) ->
-    X = dat:extract(Line, "move (\\d+) from (\\d+) to (\\d+)|.*"),
-    if
-        length(X) =:= 3 ->
-            [list_to_integer(A) || A <- X];
-        true ->
-            X
-    end.
+split(List) ->
+    split(List, [], []).
+split([], Buffer, Sections) ->
+    Sections ++ [Buffer];
+split([H | T], Buffer, Sections) when H =:= <<>> ->
+    split(T, [], Sections ++ [Buffer]);
+split([H | T], Buffer, Sections) ->
+    split(T, Buffer ++ [H], Sections).
 
-% Note: I am currently cheating! Sorry :O
-% I simply don't have time this morning to parse the data
-% I shall finish this later
-% In the meantime I have hard-coded my starting stacks and completed the moving part of the challenge
+extract_data(Dat) ->
+    [StackData, MoveData] = split(Dat),
+    {process_stack_data(StackData), process_move_data(MoveData)}.
 
-num_stacks() -> 9.
+build_stacks([], Stacks) -> Stacks;
+build_stacks([Row | T], Stacks) -> build_stacks(T, add_row_of_crates(Row, Stacks)).
 
-cheat_stacks(1) -> "NRJTZBDF";
-cheat_stacks(2) -> "HJNSR";
-cheat_stacks(3) -> "QFZGJNRC";
-cheat_stacks(4) -> "QTRGNVF";
-cheat_stacks(5) -> "FQTL";
-cheat_stacks(6) -> "NGRBZWCQ";
-cheat_stacks(7) -> "MHNSLCF";
-cheat_stacks(8) -> "JTMQND";
-cheat_stacks(9) -> "SGP".
+add_row_of_crates(Row, Stacks) -> add_row_of_crates(Row, Stacks, []).
+add_row_of_crates([], [], Added) ->
+    Added;
+add_row_of_crates([Crate | TCrate], [], Added) ->
+    % add empty stacks as necessary
+    add_row_of_crates([Crate | TCrate], [[]], Added);
+add_row_of_crates([Crate | TCrate], [Stack | TStack], Added) ->
+    add_row_of_crates(TCrate, TStack, Added ++ [add_crate(Crate, Stack)]).
+
+add_crate(Crate, Stack) when [Crate] =:= " " -> Stack;
+add_crate(Crate, Stack) -> Stack ++ [Crate].
+
+process_stack_data(Dat) ->
+    % Get the Row data (extract the Crates)
+    RowData = lists:sublist(Dat, length(Dat) - 1),
+    ExtractCrates = fun(X) -> lists:flatten(dat:extract_global(X, ".(.). ?")) end,
+    Rows = [ExtractCrates(X) || X <- RowData],
+    % Build the stacks
+    build_stacks(Rows, []).
+
+process_move_data(Dat) ->
+    [dat:to_integer_list(X) || X <- Dat].
 
 run_step_9001(S, [Num, From, To]) -> run_step_9001(S, From, To, [], Num).
 run_step_9001(S, _, To, Buffer, 0) ->
@@ -56,14 +69,13 @@ run_plan(F, S, [M | T]) ->
     S1 = F(S, M),
     run_plan(F, S1, T).
 
-top_of_each_stack(Stacks) -> [T || [T|_Rest] <- Stacks].
+top_of_each_stack(Stacks) -> [T || [T | _Rest] <- Stacks].
 
 answer() ->
-    Dat = aoc22:data("05", fun process_dat/1),
-    Moves = [D || D <- Dat, length(D) =:= 3],
-    StartStacks = [cheat_stacks(I) || I <- lists:seq(1, num_stacks())],
-    Stacks1 = run_plan(fun run_step_9000/2, StartStacks, Moves),
+    Dat = aoc22:data("05", fun dat:nop/1),
+    {Stacks, Moves} = extract_data(Dat),
+    Stacks1 = run_plan(fun run_step_9000/2, Stacks, Moves),
     Tops1 = top_of_each_stack(Stacks1),
-    Stacks2 = run_plan(fun run_step_9001/2, StartStacks, Moves),
+    Stacks2 = run_plan(fun run_step_9001/2, Stacks, Moves),
     Tops2 = top_of_each_stack(Stacks2),
-    {Tops1 , Tops2}.
+    {Tops1, Tops2}.
