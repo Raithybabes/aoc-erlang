63c432cce624898a6ac5223be95684c3f16c19f5
Raith Munro <raith.munro@vonage.com>
2022-12-06 18:33:02 +0000

    AOC22 06 improvement. Is it?

--- src/aoc_2022/aoc22_06.erl
+++ src/aoc_2022/aoc22_06.erl
@@ -2,22 +2,14 @@
 
 -export([answer/0]).
 
-has_no_dupes(L) -> length(lists:uniq(L)) =:= length(L).
-
-% find_start_of_packet(L) -> find_start_of_packet(L, 0).
-% find_start_of_packet([H1, H2, H3, H4 | T], Count) ->
-%     MarkerFound = has_no_dupes([H1, H2, H3, H4]),
-%     if
-%         MarkerFound -> Count + 4;
-%         true -> find_start_of_packet([H2, H3, H4 | T], Count + 1)
-%     end.
-
-find_uniq_seq(List, Length) -> find_uniq_seq(List, Length, 0).
-find_uniq_seq([_H | T] = List, Length, Pos) ->
-    Found = has_no_dupes(rwm:first(Length, List)),
+find_uniq_seq(List, Length) -> find_uniq_seq(List, Length, [], 0).
+find_uniq_seq([H | T], Length, Buffer, Pos) when length(Buffer) < Length ->
+    find_uniq_seq(T, Length, Buffer ++ [H], Pos + 1);
+find_uniq_seq([H | T], Length, [_ | TBuffer] = Buffer, Pos) ->
+    Unique = lists:uniq(Buffer) =:= Buffer,
     if
-        Found -> Pos + Length;
-        true -> find_uniq_seq(T, Length, Pos + 1)
+        Unique -> Pos;
+        true -> find_uniq_seq(T, Length, TBuffer ++ [H], Pos + 1)
     end.
 
 process_dat(Line) -> binary_to_list(Line).
