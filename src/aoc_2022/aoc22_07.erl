6e488f0dc4643e7cc8ab38b9ee54a1a1d937be8c
Raith Munro <raith.munro@vonage.com>
2022-12-07 12:25:11 +0000

    aoc22 07 removed debug code

--- src/aoc_2022/aoc22_07.erl
+++ src/aoc_2022/aoc22_07.erl
@@ -8,6 +8,12 @@
 new_file(Name, Size) -> #file{name = Name, size = list_to_integer(Size)}.
 
 new_dir(Name) -> #dir{name = Name}.
+
+new_or_existing_dir([], DirName) ->
+    [new_dir(DirName)];
+new_or_existing_dir(Existing, _) ->
+    Existing.
+
 add_dir(DirName, R) ->
     Existing = [D || D <- R#dir.dirs, D#dir.name =:= DirName],
     R#dir{dirs = R#dir.dirs ++ new_or_existing_dir(Existing, DirName)}.
@@ -15,106 +21,54 @@ add_file(File, R) ->
     Existing = [F || F <- R#dir.files, F#file.name =:= File#file.name],
     R#dir{files = (R#dir.files -- Existing) ++ [File]}.
 
-new_or_existing_dir([], DirName) ->
-    [new_dir(DirName)];
-new_or_existing_dir(Existing, _) ->
-    Existing.
-
-process_dat(Line) -> binary_to_list(Line).
-
-% process_line([$$, $\s, $c, $d, $\s, $/]) -> "ROOT";
-% process_line([$$, $\s, $c, $d, $\s, $., $.]) -> "UP";
-% process_line([$$, $\s, $c, $d, $\s | X]) -> "DOWN " ++ X;
-% process_line([$$, $\s, $l, $s]) -> "LIST";
-% process_line([$$, $\s | T]) -> "UNSUPPORTED COMMAND " ++ T;
-% process_line([$d,$i,$r,$\s|X]) -> "-- DIR " ++ X;
-% process_line(X) -> "-- FILE " ++ X.
-
-test_data() ->
-    [
-        "$ cd /",
-        "$ ls",
-        "dir a",
-        "14848514 b.txt",
-        "8504156 c.dat",
-        "dir d",
-        "$ cd a",
-        "$ ls",
-        "dir e",
-        "29116 f",
-        "2557 g",
-        "62596 h.lst",
-        "$ cd e",
-        "$ ls",
-        "584 i",
-        "$ cd ..",
-        "$ cd ..",
-        "$ cd d",
-        "$ ls",
-        "4060174 j",
-        "8033020 d.log",
-        "5626152 d.ext",
-        "7214296 k"
-    ].
-
 process(Lines) ->
-    {_, FS} = process(Lines, null, 1),
+    {_, FS} = process(Lines, null),
     FS.
-process([], Cwd, _Pos) ->
-    % io:format("~p EXIT ~p~n", [Pos, Cwd]),
+process([], Cwd) ->
+    % Finished
     {[], Cwd};
-process([[$$, $\s, $c, $d, $\s, $/] | T], _Cwd, Pos) ->
-    % ROOT
-    % io:format("~p ROOT ~p~n", [Pos, Cwd]),
-    process(T, new_dir("/"), Pos + 1);
-process([[$$, $\s, $c, $d, $\s, $., $.] | T], Cwd, _Pos) ->
-    % UP
-    % io:format("~p UP ~p~n", [Pos, Cwd]),
+process([[$$, $\s, $c, $d, $\s, $/] | T], _Cwd) ->
+    % cd /
+    process(T, new_dir("/"));
+process([[$$, $\s, $c, $d, $\s, $., $.] | T], Cwd) ->
+    % cd ..
     {T, Cwd};
-process([[$$, $\s, $c, $d, $\s | Name] | T], Cwd, Pos) ->
-    % DOWN
-    % io:format("~p DOWN ~p~p~n", [Pos, Name, Cwd]),
+process([[$$, $\s, $c, $d, $\s | Name] | T], Cwd) ->
+    % cd <DirName>
     Dirs = Cwd#dir.dirs,
-    % io:format("-- Dirs ~p~n", [Dirs]),
     [Sub] = [D || D <- Dirs, D#dir.name =:= Name],
-    {T1, Sub1} = process(T, Sub, Pos + 1),
+    {T1, Sub1} = process(T, Sub),
     Cwd1 = Cwd#dir{dirs = (Dirs -- [Sub]) ++ [Sub1]},
-    process(T1, Cwd1, Pos + 1);
-process([[$$, $\s, $l, $s] | T], Cwd, Pos) ->
-    % LIST
-    % if
-    %     length(Cwd#dir.files) + length(Cwd#dir.dirs) > 0 ->
-    %         io:format("~p LIST !WARNING! Cwd has already been populated ~p~n", [Pos, Cwd]);
-    %     true ->
-    %         io:format("~p LIST ~p~n", [Pos, Cwd])
-    % end,
-    process(T, Cwd, Pos + 1);
-process([[$$, $\s | T] | T], _Cwd, Pos) ->
-    io:format("~p UNSUPPORTED COMMAND ~p~n", [Pos, T]),
-    {error, Pos, T};
-process([[$d, $i, $r, $\s | DirName] | T], Cwd, Pos) ->
-    % DIR X
-    % io:format("~p ++ DIR ~p ~p~n", [Pos, DirName, Cwd]),
-    process(T, add_dir(DirName, Cwd), Pos + 1);
-process([FileDetails | T], Cwd, Pos) ->
-    % FILE X
-    % io:format("~p ++ FILE ~p ~p~n", [Pos, FileDetails, Cwd]),
+    process(T1, Cwd1);
+process([[$$, $\s, $l, $s] | T], Cwd) ->
+    % ls
+    process(T, Cwd);
+process([[$$, $\s | T] | T], _Cwd) ->
+    % <SomethingUnsupported>
+    io:format("UNSUPPORTED COMMAND ~p~n", [T]),
+    {error, T};
+process([[$d, $i, $r, $\s | DirName] | T], Cwd) ->
+    % dir <DirName>
+    process(T, add_dir(DirName, Cwd));
+process([FileDetails | T], Cwd) ->
+    % <FileSize> <FileName>
     [Size, Name] = dat:extract(FileDetails, "(\\d+) (.+)"),
     File = new_file(Name, Size),
-    process(T, add_file(File, Cwd), Pos + 1).
+    process(T, add_file(File, Cwd)).
 
 dir_size(Dir) ->
-    Subs = [dir_size(D) || D <- Dir#dir.dirs],
+    SubDirs = [dir_size(D) || D <- Dir#dir.dirs],
     Size =
         lists:sum([F#file.size || F <- Dir#dir.files]) +
-            lists:sum([SubSize || {_, SubSize} <- Subs]),
+            lists:sum([SubSize || {_, SubSize} <- SubDirs]),
     {Dir#dir.name, Size}.
 
 walk_fs(Dir, F) ->
     [F(Dir)] ++ lists:flatten([walk_fs(D, F) || D <- Dir#dir.dirs]).
 
+process_dat(Line) -> binary_to_list(Line).
+
 answer() ->
-    % Dat = test_data(),
     Dat = aoc22:data("07", fun process_dat/1),
     FS = process(Dat),
     {_, RootBytes} = RootSize = dir_size(FS),
